2c30cfce7594d696d31b28a95c946b7fc9438bfa
Joaquin Talice <joaquintalice@gmail.com>
2023-12-09 22:02:26 -0300

    feat: db connection & migrations && code first approach

---
+++ demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options){}
+
+        public DbSet<Shirt> Shirts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Data seeding
+            modelBuilder.Entity<Shirt>().HasData(
+                    new Shirt() { Id = 1, Brand = "Nike", Color = "red", Gender = "men", Size = 8, Price = 49.99 },
+                    new Shirt() { Id = 2, Brand = "Adidas", Color = "blue", Gender = "women", Size = 7, Price = 39.99 },
+                    new Shirt() { Id = 3, Brand = "Puma", Color = "green", Gender = "men", Size = 12, Price = 29.99 },
+                    new Shirt() { Id = 4, Brand = "Reebok", Color = "black", Gender = "women", Size = 6, Price = 59.99 },
+                    new Shirt() { Id = 5, Brand = "Under Armour", Color = "gray", Gender = "men", Size = 9, Price = 79.99 },
+                    new Shirt() { Id = 6, Brand = "Fila", Color = "white", Gender = "women", Size = 7, Price = 34.99 },
+                    new Shirt() { Id = 7, Brand = "New Balance", Color = "purple", Gender = "men", Size = 10, Price = 54.99 },
+                    new Shirt() { Id = 8, Brand = "Asics", Color = "yellow", Gender = "women", Size = 11, Price = 44.99 }
+                );
+        }
+    }
+}
---
+++ demo/Migrations/20231209184623_init.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shirts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Size = table.Column<int>(type: "int", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shirts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Shirts",
+                columns: new[] { "Id", "Brand", "Color", "Gender", "Price", "Size" },
+                values: new object[,]
+                {
+                    { 1, "Nike", "red", "men", 49.990000000000002, 8 },
+                    { 2, "Adidas", "blue", "women", 39.990000000000002, 7 },
+                    { 3, "Puma", "green", "men", 29.989999999999998, 12 },
+                    { 4, "Reebok", "black", "women", 59.990000000000002, 6 },
+                    { 5, "Under Armour", "gray", "men", 79.989999999999995, 9 },
+                    { 6, "Fila", "white", "women", 34.990000000000002, 7 },
+                    { 7, "New Balance", "purple", "men", 54.990000000000002, 10 },
+                    { 8, "Asics", "yellow", "women", 44.990000000000002, 11 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shirts");
+        }
+    }
+}
